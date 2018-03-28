d8b9c897c097cd65394ebe9b789736704bada7cb
Tyler Jones <tylerjones64@gmail.com>
2018-03-28 01:47:54 -0700

    Cleanup package.json

--- scripts/prepare-lib.js
+++ scripts/prepare-lib.js
@@ -9,11 +9,11 @@ const libPath = path.join(rootPath, 'lib');
 const package = require(path.join(rootPath, 'package.json'));
 
 delete package.scripts;
-delete package.optionalDependencies;
 delete package.devDependencies;
 delete package.jest;
 
 package.main = './index.js';
+package.types = './index.d.ts';
 
 fs.writeFile(
   path.join(libPath, 'package.json'),
